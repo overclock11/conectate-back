e65578da1dad848c75c7b64020f7083d0d05d0af
Sergio Pérez <se.perezf@uniandes.edu.co>
2018-03-30 14:57:04 -0500

    Eliminar ambiente

--- ambiente/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\usuario\documents\conectate\conectate\conectate-back\ambiente\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
