2b99a8d5b48eee06fae15063c5d17dc220638a1d
Julian <jlian92@hotmail.com>
2018-03-31 14:23:01 -0500

    [conectate-12] - Ajustes para el filtro por sistema operativo

--- conectate_back/conectate_back/settings.py
+++ conectate_back/conectate_back/settings.py
@@ -105,8 +105,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
