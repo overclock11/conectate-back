e90fda235aaee0f7dff1a9fbfc5f77539391b4c5
Wendell Arias <wdarias@unicesar.edu.co>
2018-03-31 16:47:30 -0500

    ajuste guardar tutorial

--- conectate_back/apps/home/views.py
+++ conectate_back/apps/home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from apps.home.models import *
 from apps.home.serializers import *
 from rest_framework import generics
+from rest_framework.utils import json
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -39,3 +40,10 @@ class TutorialList(generics.ListCreateAPIView):
         toolId = self.request.GET['toolId']
         st = st.filter(tool_id = toolId)
         return st
+
+    def create(self, request, *args, **kwargs):
+        data = json.loads(request.body)
+        tool = Tool.objects.get(id=data['tool'])
+        data['tool'] = tool
+        print(tool)
+        return Tutorial.objects.create(**data)
