8263c7b06cda3bb3e342803f0cd4ad4cd1b7f9e3
Sergio Pérez <se.perezf@uniandes.edu.co>
2018-04-01 20:23:35 -0500

    Remover ambiente

---
+++ conectate_back/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/sergio/Documents/Projects/Uniandes/Conectate/conectate-back/conectate_back/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
