8f854dfaf00b62c1c5588ba5499717f440070450
jramell <sergiomaderarangel@gmail.com>
2018-04-19 17:55:58 -0500

    default db es postgres, django-extensions removido

--- conectate_back/settings.py
+++ conectate_back/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'apps.home',
     'corsheaders',
-    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -81,7 +80,7 @@ WSGI_APPLICATION = 'conectate_back.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'postgres': {
+    'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get('Database'),
         'USER': os.environ.get('User'),
@@ -89,7 +88,7 @@ DATABASES = {
         'HOST': os.environ.get('Host'),
         'PORT': os.environ.get('Port'),
     },    
-    'default': {
+    'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
