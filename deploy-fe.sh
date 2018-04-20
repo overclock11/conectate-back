323bf2b87bdbea2c9715b290252d33707245b108
Sergio Pérez <se.perezf@uniandes.edu.co>
2018-04-20 03:26:54 -0500

    Add node postinstall

--- deploy-fe.sh
+++ deploy-fe.sh
@@ -2,6 +2,4 @@
 
 git clone -b develop https://github.com/overclock11/conectate-front.git frontend/conectate-front
 cd frontend/conectate-front 
-nodeenv node
-source node/bin/activate
 (npm i && ./node_modules/@angular/cli/bin/ng build --output-path ../public)
