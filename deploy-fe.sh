4d5e0b7bd39ad62b662fab7c60c66a0dd3b055c4
Sergio Pérez <se.perezf@uniandes.edu.co>
2018-04-20 10:11:30 -0500

    Cambios en deploy

--- deploy-fe.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-git clone -b develop https://github.com/overclock11/conectate-front.git frontend/conectate-front
-(cd frontend/conectate-front && npm i && npm run "ng build --output-path ../public")
