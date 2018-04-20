afa06eea70000d0ac40ff309eb5aa6e53b45a791
Sergio Pérez <se.perezf@uniandes.edu.co>
2018-04-20 03:33:54 -0500

    Add ng

--- deploy-fe.sh
+++ deploy-fe.sh
@@ -2,4 +2,4 @@
 
 git clone -b develop https://github.com/overclock11/conectate-front.git frontend/conectate-front
 cd frontend/conectate-front 
-(npm i && ./node_modules/@angular/cli/bin/ng build --output-path ../public)
+(npm i && ng build --output-path ../public)
